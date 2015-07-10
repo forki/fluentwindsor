c7483367af5094ea176a9012d8111e8e98386867
Gav <fir3pho3nixx@gmail.com>
2015-07-10 15:01:30 +0100

    .

--- package-and-push.bat
+++
@@ -1,9 +0,0 @@
-rm -rf *.nupkg
-
-nuget pack ./FluentWindsor/nuget.nuspec
-nuget pack ./FluentWindsor.Mvc/nuget.nuspec
-nuget pack ./FluentWindsor.WebApi/nuget.nuspec
-
-nuget push ./FluentWindsor.1.0.0.*.nupkg
-nuget push ./FluentWindsor.Mvc.1.0.0.*.nupkg
-nuget push ./FluentWindsor.WebApi.1.0.0.*.nupkg
