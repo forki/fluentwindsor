097fafbdf38ab8751277323133bd4bfeef902af3
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2016-05-03 21:42:25 +0100

    For bubbling relfection type load exceptions to the top

--- FluentWindsor/Extensions/AssemblyExtensions.cs
+++ FluentWindsor/Extensions/AssemblyExtensions.cs
@@ -15,32 +15,52 @@ namespace FluentlyWindsor.Extensions
                 foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
                 {
                     var interfaces = type.GetInterfaces();
-                    if (interfaces.Any(x => x.FullName == typeof (T).FullName))
+                    if (interfaces.Any(x => x.FullName == typeof(T).FullName))
                         return true;
                 }
             }
-            catch {}
+            catch (ReflectionTypeLoadException err)
+            {
+                var loaderErrors = string.Join(",", err.LoaderExceptions.Select(x => x.ToString()));
+                throw new Exception(loaderErrors, err);
+            }
             return false;
         }
 
         public static Type[] GetAnyTypeThatImplementsInterface<T>(this Assembly assembly, IAssemblyScanningPolicy[] policies)
         {
             var results = new List<Type>();
-            foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+            try
             {
-                var interfaces = type.GetInterfaces();
-                if (interfaces.Any(x => x.FullName == typeof(T).FullName))
-                    results.Add(type);
+                foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+                {
+                    var interfaces = type.GetInterfaces();
+                    if (interfaces.Any(x => x.FullName == typeof(T).FullName))
+                        results.Add(type);
+                }
+            }
+            catch (ReflectionTypeLoadException err)
+            {
+                var loaderErrors = string.Join(",", err.LoaderExceptions.Select(x => x.ToString()));
+                throw new Exception(loaderErrors, err);
             }
             return results.ToArray();
         }
 
         public static bool HasAnyTypeThatIsSubClassOf<T>(this Assembly assembly, IAssemblyScanningPolicy[] policies)
         {
-            foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+            try
             {
-                if (type.IsSubclassOf(typeof (T)))
-                    return true;
+                foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+                {
+                    if (type.IsSubclassOf(typeof(T)))
+                        return true;
+                }
+            }
+            catch (ReflectionTypeLoadException err)
+            {
+                var loaderErrors = string.Join(",", err.LoaderExceptions.Select(x => x.ToString()));
+                throw new Exception(loaderErrors, err);
             }
             return false;
         }
@@ -48,20 +68,36 @@ namespace FluentlyWindsor.Extensions
         public static Type[] GetAnyTypeThatIsSubClassOf<T>(this Assembly assembly, IAssemblyScanningPolicy[] policies)
         {
             var results = new List<Type>();
-            foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+            try
             {
-                if (type.IsSubclassOf(typeof(T)))
-                    results.Add(type);
+                foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+                {
+                    if (type.IsSubclassOf(typeof(T)))
+                        results.Add(type);
+                }
+            }
+            catch (ReflectionTypeLoadException err)
+            {
+                var loaderErrors = string.Join(",", err.LoaderExceptions.Select(x => x.ToString()));
+                throw new Exception(loaderErrors, err);
             }
             return results.ToArray();
         }
 
         public static bool HasAnyTypeThatIs<T>(this Assembly assembly, IAssemblyScanningPolicy[] policies)
         {
-            foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+            try
             {
-                if (type.FullName == typeof(T).FullName)
-                    return true;
+                foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+                {
+                    if (type.FullName == typeof(T).FullName)
+                        return true;
+                }
+            }
+            catch (ReflectionTypeLoadException err)
+            {
+                var loaderErrors = string.Join(",", err.LoaderExceptions.Select(x => x.ToString()));
+                throw new Exception(loaderErrors, err);
             }
             return false;
         }
@@ -69,10 +105,18 @@ namespace FluentlyWindsor.Extensions
         public static Type[] GetAnyTypeThatIs<T>(this Assembly assembly, IAssemblyScanningPolicy[] policies)
         {
             var results = new List<Type>();
-            foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+            try
             {
-                if (type.FullName == typeof(T).FullName)
-                    results.Add(type);
+                foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+                {
+                    if (type.FullName == typeof(T).FullName)
+                        results.Add(type);
+                }
+            }
+            catch (ReflectionTypeLoadException err)
+            {
+                var loaderErrors = string.Join(",", err.LoaderExceptions.Select(x => x.ToString()));
+                throw new Exception(loaderErrors, err);
             }
             return results.ToArray();
         }
@@ -80,13 +124,20 @@ namespace FluentlyWindsor.Extensions
         public static Type[] GetAnyTypeWithFullName(this Assembly assembly, IAssemblyScanningPolicy[] policies, string fullName)
         {
             var results = new List<Type>();
-            foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+            try
             {
-                if (type.FullName.ToLower() == fullName.ToLower())
-                    results.Add(type);
+                foreach (var type in assembly.GetTypes().Where(x => policies.All(y => y.IsTypeAllowed(x))))
+                {
+                    if (type.FullName.ToLower() == fullName.ToLower())
+                        results.Add(type);
+                }
+            }
+            catch (ReflectionTypeLoadException err)
+            {
+                var loaderErrors = string.Join(",", err.LoaderExceptions.Select(x => x.ToString()));
+                throw new Exception(loaderErrors, err);
             }
             return results.ToArray();
         }
-
     }
 }
