ad1ceab93d815fbc4b34584c59ab75c6a5ea05a4
Gavin van der Merwe <gavin.vandermerwe@chambersandpartners.co.uk>
2017-02-04 18:02:42 +0000

    .

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,5 @@
 msbuild  .\.build\BuildTasks\BuildTasks.csproj /p:Configuration=Release
+SLEEP 1
 msbuild .build\build-and-deploy.msbuild /p:Properties=build-and-deploy.properties.msbuild
 git add --all
 git commit -am " -- Nuget Deploy -- "
