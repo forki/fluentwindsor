001bcd3dbd6a4fe49eca241cb7a4c09b8ccc293a
Gavin van der Merwe <gavin.vandermerwe@chambersandpartners.co.uk>
2017-02-04 18:04:52 +0000

    .

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,6 @@
+.\.paket\paket.bootstrapper.exe
+.\.paket\paket.exe install
 msbuild  .\.build\BuildTasks\BuildTasks.csproj /p:Configuration=Release
-SLEEP 1
 msbuild .build\build-and-deploy.msbuild /p:Properties=build-and-deploy.properties.msbuild
 git add --all
 git commit -am " -- Nuget Deploy -- "
