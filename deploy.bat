ec48d5c4545de94452683c4fafe2fa2e6af6e812
Gavin van der Merwe <gavin.vandermerwe@chambersandpartners.co.uk>
2017-02-04 15:31:17 +0000

    Have project build from the command line

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 msbuild .build\build-and-deploy.msbuild /p:Properties=build-and-deploy.properties.v4.5.msbuild
-rem git add --all
-rem git commit -am " -- Nuget Deploy -- "
-rem git push
+git add --all
+git commit -am " -- Nuget Deploy -- "
+git push
