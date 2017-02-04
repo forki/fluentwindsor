bff246a9f15574744232ab995834cfefb6082a39
Gavin van der Merwe <gavin.vandermerwe@chambersandpartners.co.uk>
2017-02-04 17:59:34 +0000

    Fixed build scripts

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
+msbuild  .\.build\BuildTasks\BuildTasks.csproj /p:Configuration=Release
 msbuild .build\build-and-deploy.msbuild /p:Properties=build-and-deploy.properties.msbuild
 git add --all
 git commit -am " -- Nuget Deploy -- "
