de3972c3ed39b632aa4b2e6a56fd9058a6c45bc1
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2017-02-21 14:55:29 +0000

    Fixed assembly attributes in the test files

--- FluentWindsor.Hawkeye/Hawkeye.Tests/Properties/AssemblyInfo.cs
+++ FluentWindsor.Hawkeye/Hawkeye.Tests/Properties/AssemblyInfo.cs
@@ -1,14 +1,3 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
+
 [assembly: AssemblyTitle("FluentWindsor.Hawkeye.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FluentWindsor.Hawkeye.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("443b1356-e54b-488c-94a5-5092a421a2f7")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
