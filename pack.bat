af2bf36d0b246335b28d4852dddba42100eecdbc
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2017-04-01 23:15:30 +0100

    More paket clean up

--- pack.bat
+++
@@ -1,4 +0,0 @@
-.\.paket\paket.bootstrapper.exe
-.\.paket\paket.exe install
-msbuild  .\.build\BuildTasks\BuildTasks.csproj /p:Configuration=Release
-msbuild .build\build-and-deploy.msbuild /p:Properties=build-and-deploy.properties.msbuild /t:tasks\pack
