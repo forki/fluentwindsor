043b7c8772daf0164086ff09e92c1ec08529e364
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2017-04-04 20:41:21 +0100

    Have example web working with MVC and WebApi, going to update the docs

---
+++ .examples/FluentWindsor/Example.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Example.Test.AssemblyC;
+
+namespace Example.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ServiceC _serviceC;
+
+        public HomeController(ServiceC serviceC)
+        {
+            _serviceC = serviceC;
+        }
+
+        public ActionResult Index()
+        {
+            _serviceC.Execute();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ .examples/FluentWindsor/Example.Web/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using FluentlyWindsor;
+using FluentlyWindsor.Mvc;
+using FluentlyWindsor.WebApi;
+
+namespace Example.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            FluentWindsor
+                .NewContainer(Assembly.GetExecutingAssembly())
+                .WithArrayResolver()
+                .WithInstallers()
+                .RegisterApiControllers(GlobalConfiguration.Configuration)
+                .RegisterMvcControllers(ControllerBuilder.Current, "Example.Web.Controllers")
+                .Create();
+        }
+    }
+}
