494f7ae92a86129a2e19501f9869d3f287617aa6
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2017-04-04 21:09:55 +0100

    Think I meant to introduce fake here and accidentally deleted the mega legacy bat files

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+.\.paket\paket.bootstrapper.exe
+.\.paket\paket.exe install
+msbuild  .\.build\BuildTasks\BuildTasks.csproj /p:Configuration=Release
+msbuild .build\build-and-deploy.msbuild /p:Properties=build-and-deploy.properties.msbuild
+git add --all
+git commit -am " -- Nuget Deploy -- "
+git push
